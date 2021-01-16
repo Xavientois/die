7d0d77daf0f13fbc0325e28d326d98ef3678e123
Xavientois <joshua.rampersad@hotmail.com>
2021-01-15 21:53:49 -0500

    Add integration tests

---
+++ tests/integration_test.rs
@@ -0,0 +1,20 @@
+extern crate die_exit;
+use die_exit::*;
+
+#[test]
+#[should_panic]
+fn die_with_print_selector() {
+    die!("Escape invalid character: '{}'", '_'; 42);
+}
+
+#[test]
+#[should_panic]
+fn die_with_code_first() {
+    die!(42; "Escape invalid character");
+}
+
+#[test]
+#[should_panic]
+fn die_empty() {
+    die!();
+}
